5474ecc3d946b561152c7b148f9da910a00f4678
Krzysiek <kgrygiel@gmail.com>
2019-07-29 10:43:57 +0200

    Update column.js

--- js/column.js
+++ js/column.js
@@ -25,7 +25,7 @@ function Column(id, name, color) {
         data.append('name', cardName);
         data.append('bootcamp_kanban_column_id', self.id);
 
-        fetch(baseUrl + '/card', {
+        fetch(prefix + baseUrl + '/card', {
             method: 'POST',
             headers: myHeaders,
             body: data,
